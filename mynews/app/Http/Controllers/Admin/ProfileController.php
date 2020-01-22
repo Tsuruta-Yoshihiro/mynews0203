6b52fca13ba9b0e7ba37f28ad9ce43b2eef0c2db
”Tsuruta_Yoshihiro” <ytsuruta720@gmail.com>
2020-01-22 13:54:42 +0000

    mynews0122

--- mynews/app/Http/Controllers/Admin/ProfileController.php
+++ mynews/app/Http/Controllers/Admin/ProfileController.php
@@ -19,26 +19,44 @@ class ProfileController extends Controller
 public function create(Request $request)
     {
       
-      $this->validate($request, Profile::$rules);
-      $profile = new Profile;
-      $form = $request->all();
+        $this->validate($request, Profile::$rules);
+        $profile = new Profile;
+        $form = $request->all();
       
-      unset($form['_token']);
+        unset($form['_token']);
       
-      $profile->fill($form);
-      $profile->save();
+        $profile->fill($form);
+        $profile->save();
       
-      return redirect('admin/profile/create');
+        return redirect('admin/profile/create');
     }
     
-    public function edit()
+    
+    public function edit(Request $request)
     {
-      return view('admin.profile.edit');
+        $profile = Profile::find($request->id);
+        if (empty($profile)) {
+          abort(404);
+        }
+        return view('admin.profile.edit', ['profile_form' => $profile]);
     }
     
-    public function update()
+    public function update(Request $request)
     {
-      return redirect('admin/profile/edit');
+        $this->validate($request, Profile::$rules);
+        $profile = Profile::find($request->id);
+        $profile_form = $request->all();
+        if (isset($profile_form['image'])) {
+          $path = $request->file('image')->store('public/image');
+          $profile->image_path = basename($path);
+          unset($profile_form['image']);
+        } elseif (isset($request->remove)) {
+          $profile->image_path = null;
+          unset($profile_form['remove']);
+        }
+        
+        unset($profile_form['_token']);
+        $profile->fill($profile_form)->save();
+        return redirect('admin/profile');
     }
-    
 }
