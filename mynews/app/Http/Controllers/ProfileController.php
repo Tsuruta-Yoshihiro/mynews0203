ad80f008a4121907838955065033f15aef55189b
”Tsuruta_Yoshihiro” <ytsuruta720@gmail.com>
2020-02-02 09:34:43 +0000

    mynews0202_2

--- mynews/app/Http/Controllers/ProfileController.php
+++ mynews/app/Http/Controllers/ProfileController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\HTML;
 use App\Profile;
 
 class ProfileController extends Controller
--- mynews/routes/web.php
+++ mynews/routes/web.php
@@ -22,24 +22,20 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
   Route::get('news/edit', 'Admin\NewsController@edit');
   Route::post('news/edit', 'Admin\NewsController@update');
   Route::get('news/delete', 'Admin\NewsController@delete');
-  Route::get('/', 'NewsController@index');
 });
 
 
-//課題３//
-Route::group(['prefix' => 'admin'],function(){
-  Route::get('News/create', 'Admin\AAAController@bbb');
-});
 
-
-//課題４//
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
   Route::get('profile/create', 'Admin\ProfileController@add');
   Route::post('profile/create', 'Admin\ProfileController@create');
   Route::get('profile/edit', 'Admin\ProfileController@edit');
   Route::post('profile/edit' , 'Admin\ProfileController@update');
-  Route::get('/', 'ProfileController@index');
 });
+
+
+Route::get('/', 'NewsController@index');
+Route::get('profile', 'ProfileController@index');
 
 Auth::routes();
 
